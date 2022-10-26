3e59847f5c0e83645fca6c379fe6d6456d8000fa
ryza <rickyzatnika91@gmail.com>
2022-10-26 17:16:10 +0700

    test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
